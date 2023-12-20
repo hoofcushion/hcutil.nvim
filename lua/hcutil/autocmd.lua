684d655a794ef489e23dc0b45bf24ab07a8e178b
abcdefg233 <32760059+abcdefg233@users.noreply.github.com>
2023-12-21 00:35:24 +0800

    Fix: autocmd.lua wrong require

--- lua/hcutil/autocmd.lua
+++ lua/hcutil/autocmd.lua
@@ -8,27 +8,25 @@ function M.nvim_get_augroups_by_name()
  end
  return augroups
 end
-local function fn_combine(main,sub)
+local function fn_cond_combine(main,sub)
  local new_fn=function(...)
-  sub()
-  return main(...)
+  local ret=main(...)
+  if ret then sub() end
+  return ret
  end
  return new_fn
 end
 ---@class nvim_create_autocmd_param
 ---@field [1] nvim_autocmd_event
 ---@field [2] nvim_create_autocmd_opts
----@class autocmds
----@field init function|nil
----@field func nvim_create_autocmd_param
----@field fini nvim_create_autocmd_param|nil
+---@alias autocmds {init:function}|nvim_create_autocmd_param[]
 ---@class Autocmd_Object: meta_class
 ---@field start fun(self):self
 ---@field delete fun(self):self
 ---@field group string
 ---@field autocmds autocmds
 ---@field active boolean
-local Autocmd=require("class"):__new()
+local Autocmd=require("util.class"):__new()
 ---@class Autocmd_Object_Private: Autocmd_Object
 ---@field Create_Autocmd fun(self)
 ---@field Parse_Autocmd fun(self)
@@ -37,29 +35,22 @@ M.Autocmd=Autocmd
 function Autocmd_private:Create_Autocmd()
  vim.api.nvim_create_augroup(self.group,{})
  local autocmds=self.autocmds
- local init,func,fini=autocmds.init,autocmds.func,autocmds.fini
+ local init=autocmds.init
  if init~=nil then
   init()
  end
- if func[2].once==true then
-  func[2].callback=fn_combine(func[2].callback,function() self:delete() end)
- end
- vim.api.nvim_create_autocmd(func[1],func[2])
- if fini~=nil then
-  fini[2].callback=fn_combine(fini[2].callback,function() self:delete() end)
-  vim.api.nvim_create_autocmd(fini[1],fini[2])
+ for _,autocmd in ipairs(autocmds) do
+  if autocmd[2].once~=true then
+   autocmd[2].callback=fn_cond_combine(autocmd[2].callback,function() self:delete() end)
+  end
+  vim.api.nvim_create_autocmd(autocmd[1],autocmd[2])
  end
 end
 function Autocmd_private:Parse_Autocmd()
  local autocmds=self.autocmds
- local func=autocmds.func
- if func[2].group==nil then
-  func[2].group=self.group
- end
- local fini=autocmds.fini
- if fini~=nil then
-  if fini[2].group==nil then
-   fini[2].group=self.group
+ for _,autocmd in ipairs(autocmds) do
+  if autocmd[2].group==nil then
+   autocmd[2].group=self.group
   end
  end
 end
